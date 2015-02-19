1e6026b4f1904aace63477cc1979fef7d8d00ad3
Richard-Swingler <rs14g12@soton.ac.uk>
2015-02-19 00:07:17 +0000

    Added features carousel (gui)
    Beginning to implement functionality
    (detection of room axies, snap back on incorect)

--- www/js/floor-creator.js
+++ www/js/floor-creator.js
@@ -2,51 +2,25 @@ angular.module('floor-creator.controllers', [])
 
 .controller('FloorCtrl', function($scope) {
   var editor = new Phaser.Game(1024, 705, Phaser.AUTO, 'canvas', { preload: preload, create: create, update:update, render:render}, false);
-  var logo, bottom_left, create_button, box, bmd, shadow, recX, recY, orX, orY, save_button; //initialise global variables [TODO] Replace by this. when eventually using states
+  var logo, create_button, floor, bmd, shadow, grid, windows, plug, carouselBg, recX, recY, orX, orY, save_button, carousel, door, doorShadow; //initialise global variables [TODO] Replace by this. when eventually using states
   function preload(){
     editor.load.image('logo', 'img/ionic.png');
     editor.load.image('grid', 'img/grid.png');
-    editor.load.image('bottom_right', 'img/bottom_right.png');
-    editor.load.image('bottom_left', 'img/bottom_left.png');
-    editor.load.image('top_left', 'img/top_left.png');
-    editor.load.image('top_right', 'img/top_right.png');
     editor.load.image('create_button', 'img/create_box.png');
     editor.load.image('save_button', 'img/save.png');
+    editor.load.image('door', 'img/door.png');
+    editor.load.image('window', 'img/window.png');
+    editor.load.image('plug', 'img/plug.png');
   }
   function create(){
     //scaling options
-    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;    
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     //have the game centered horizontally
     this.scale.pageAlignHorizontally = true;
     this.scale.pageAlignVertically = true;
     editor.stage.backgroundColor = '#ffffff'; 
-    editor.add.tileSprite(0, 0, 1024, 705, "grid");  
-    top_left = editor.add.button(editor.world.width - 200, editor.world.centerY - 100, 'top_left', function(){
-      console.log(box.x);
-      logo.x = box.x;    
-      logo.y = box.y; 
-      logo.bringToTop();     
-    }, this, 2, 1, 0);
-    top_left.visible = false;
-    bottom_left = editor.add.button(editor.world.width - 200, editor.world.centerY - 50, 'bottom_left', function(){
-      logo.x = box.x;    
-      logo.y = box.y + box.height - logo.height;  
-      logo.bringToTop();     
-    }, this, 2, 1, 0);
-    bottom_left.visible = false;
-    bottom_right = editor.add.button(editor.world.width - 200, editor.world.centerY + 50, 'bottom_right', function(){
-      logo.x = box.x + box.width - logo.width;    
-      logo.y = box.y + box.height - logo.height;  
-      logo.bringToTop();     
-    }, this, 2, 1, 0);
-    bottom_right.visible = false;
-    top_right = editor.add.button(editor.world.width - 200, editor.world.centerY, 'top_right', function(){
-      logo.x = box.x + box.width - logo.width;    
-      logo.y = box.y;  
-      logo.bringToTop();     
-    }, this, 2, 1, 0);
-    top_right.visible = false;
-    create_button = editor.add.button(editor.world.width - 200, editor.world.height -100, 'create_button', createButton, this, 2, 1, 0);
+    grid = editor.add.tileSprite(0, 0, 1024, 705, "grid"); 
+    create_button = editor.add.button(editor.world.width - 200, editor.world.height - 100, 'create_button', createButton, this, 2, 1, 0);
     create_button.on = false;
     function createButton(){
       create_button.on = !create_button.on; 
@@ -58,6 +32,7 @@ angular.module('floor-creator.controllers', [])
     }
     logo = editor.add.sprite(0, 0, 'logo');
     editor.physics.enable(logo, Phaser.Physics.ARCADE);
+    createCarousel();
     logo.body.collideWorldBounds = true;
     logo.body.bounce.setTo(1, 1);
     logo.inputEnabled = true;
@@ -68,7 +43,6 @@ angular.module('floor-creator.controllers', [])
       if(editor.input.activePointer.justPressed(50)){
         orX = editor.input.activePointer.x;
         orY = editor.input.activePointer.y;
-        //console.log(orX);
       }else{
         recX = editor.input.activePointer.x - orX;
         recY = editor.input.activePointer.y - orY;
@@ -95,21 +69,25 @@ angular.module('floor-creator.controllers', [])
       bmd.ctx.rect(0,0,recX,recY);
       bmd.ctx.fillStyle = '#ffffff';
       bmd.ctx.fill(); 
-      //creates box sprite from shadow
-      shadow = null;
-      box = editor.add.sprite(orX, orY, bmd);
-      box.inputEnabled = true;
-      box.input.enableDrag();
-      box.input.enableSnap(50, 50, false, true);
+      //creates floor sprite from shadow
+      shadow = null; //removes shadow
+      floor = editor.add.sprite(orX, orY, bmd);
+      floor.inputEnabled = true;
+      floor.input.enableDrag();
+      floor.input.enableSnap(50, 50, false, true);
       create_button.on = false; 
       create_button.kill();
-      window.localStorage.setItem("floor", JSON.stringify({orX: orX, orY: orY, recX: recX, recY: recY}));
-      top_left.visible = true;
-      top_right.visible = true;
-      bottom_left.visible = true;
-      bottom_right.visible = true;
+
       save_button = editor.add.button(editor.world.width - 200, editor.world.height -100, 'save_button', function(){
-        window.open("#/app/furniture");
+        window.localStorage.setItem("floor", JSON.stringify({orX: orX, orY: orY, recX: recX, recY: recY}));
+        floor.x = 100;
+        floor.y = 50;
+        editor.world.sendToBack(floor);
+        editor.world.sendToBack(grid);
+
+        save_button.visible = false;
+        carousel.visible = true;
+        //window.open("#/app/furniture");
       }, this, 2, 1, 0);
     }
   }
@@ -122,5 +100,63 @@ angular.module('floor-creator.controllers', [])
   function round(x){
     return Math.round(x /50)*50;
   }
+  function createCarousel(){
+    carousel = editor.add.group();
+    carouselBmd = editor.add.bitmapData(editor.width, 150);
+    // setup bitmap colour
+    carouselBmd.ctx.beginPath();
+    console.log(editor.width);
+    carouselBmd.ctx.rect(0,0, editor.width, 150);
+    carouselBmd.ctx.fillStyle = '#ffffff';
+    carouselBmd.ctx.fill(); 
+    carouselBg = carousel.create(0,editor.height - 150, carouselBmd);
+    doorShadow = carousel.create(250, editor.height - 150, 'door');
+    doorShadow.height = 150;
+    doorShadow.width = 150;
+    doorShadow.alpha = 0.2;
+    door = carousel.create(250, editor.height - 150, 'door');
+    door.height = 150;
+    door.width = 150;
+    door.inputEnabled = true;
+    door.input.enableDrag();
+    door.input.enableDrag();
+    door.input.enableSnap(50, 50, true, true);
+    door.events.onInputDown.add(dragDoor, this);
+    door.events.onDragStop.add(dropDoor, this);
+    windows = carousel.create(425, editor.height - 150, 'window');
+    windows.height = 150;
+    windows.width = 150;
+    plug = carousel.create(600, editor.height - 150, 'plug');
+    plug.height = 150;
+    plug.width = 150;
+    carousel.visible = false;
+  }
+  function dragDoor(sprite, pointer){
+    console.log(sprite.x);
+    door.loadTexture('grid', 0, false);
+    door.height = 100;
+    door.width = 100;
+  }
+  function dropDoor(sprite, pointer){
+    
+    
+    if(onWall(floor, door)){
+      console.log('yay');
+      //group items
+    } else{
+      door.x = doorShadow.x;
+      door.y = doorShadow.y;
+      console.log('noooo');
+      door.loadTexture('door', 0, false);
+      door.height = 150;
+      door.width = 150;
+    }
+  }
+  function onWall(floor, feature){
+    if (feature.x >= floor.x && feature.x <= floor.width && feature.y === floor.y){
+      console.log('touching top');
+      return true;
+    }
+    return false;
+  }
 })
-
--- www/js/solve.js
+++ www/js/solve.js
@@ -2,7 +2,7 @@ angular.module('solve.controllers', [])
 
 .controller('SolveCtrl', function($scope) {
   var solveEditor = new Phaser.Game(1024, 705, Phaser.AUTO, 'solveCanvas', { preload: preload, create: create, update:update, render:render}, false);
-  var floor, furn1, logo; //initialise global variables [TODO] Replace by this. when eventually using states
+  var floor, furnitures, logo, sprites; //initialise global variables [TODO] Replace by this. when eventually using states
   function preload(){
       solveEditor.load.image('logo', 'img/ionic.png');
       solveEditor.load.image('grid', 'img/grid.png');
@@ -17,42 +17,68 @@ angular.module('solve.controllers', [])
     solveEditor.add.tileSprite(0, 0, 1024, 705, "grid"); 
 
     floor = JSON.parse(window.localStorage.getItem("floor"));
+    console.log(floor);
     var floorBmd = createBmd(floor);
     floorBmd.ctx.fillStyle = '#ffffff';
     floorBmd.ctx.fill();
     floor = solveEditor.add.sprite(50, 50, floorBmd);
+    solveEditor.physics.enable(floor, Phaser.Physics.ARCADE);
 
     // furn1 = JSON.parse(window.localStorage.getItem("furn1"));
     // var furn1Bmd = createBmd(furn1);
-    var furnitures = JSON.parse(window.localStorage.getItem('furniture'));
+    furnitures = JSON.parse(window.localStorage.getItem('furniture'));
+    sprites = solveEditor.add.group();
+    console.log('hi');
     for(var furniture in furnitures){
-        console.log(furnitures[furniture]);
-        console.log(furnitures[furniture]['recX']);
-        furnitures[furniture] = createBmd(furnitures[furniture]);
-        furnitures[furniture] = solveEditor.add.sprite(0, 0, furnitures[furniture]);
+        var furnBmd = createBmd(furnitures[furniture]);
+        furnBmd.ctx.fillStyle = '#0080ff';
+        furnBmd.shiftHSL(0.1);
+        furnBmd.ctx.fill();
+        furnitures[furniture] = sprites.create(0, 0, furnBmd);
+        solveEditor.physics.enable(furnitures[furniture], Phaser.Physics.ARCADE);
+        furnitures[furniture].body.velocity.setTo(200, 200);
+        furnitures[furniture].body.bounce.set(0.8);
+        furnitures[furniture].body.gravity.set(0, 180);
+        furnitures[furniture].inputEnabled = true;
+        // console.log(furnitures[furniture].height);
+        furnitures[furniture].input.enableDrag();
+        furnitures[furniture].input.enableSnap(50, 50, false, true); 
+        //  This sets the image bounce energy for the horizontal  and vertical vectors (as an x,y point). "1" is 100% energy return
+        furnitures[furniture].body.bounce.set(0.8);
+        furnitures[furniture].body.gravity.set(0, 180);
     }
-    
+    sprites.setAll('body.collideWorldBounds', true);
+    sprites.setAll('body.bounce.x', 1);
+    sprites.setAll('body.bounce.y', 1);
+    sprites.setAll('body.minBounceVelocity', 0);
     logo = solveEditor.add.sprite(0,0,'logo');
     logo.inputEnabled = true;
     logo.input.enableDrag();
 
   }
   function update(){
-    
+    solveEditor.physics.arcade.collide(sprites);
+    for(var furniture in furnitures){
+      if(!checkOverlap(furnitures[furniture], floor)){
+        console.log('hi');
+        solveEditor.physics.arcade.moveToXY(furnitures[furniture], floor.width/2, floor.y, 250);  
+      }
+    }    
   }
   function render(){
+    solveEditor.debug.body(sprites);
   }
   function createBmd(item){
     var bmd = solveEditor.add.bitmapData(item['recX'],item['recY']);
     bmd.ctx.beginPath();
     bmd.ctx.rect(0,0,item['recX'],item['recY']);
-    bmd.ctx.fillStyle = '#0080ff';
-    bmd.ctx.fill();
-    // var item['name'] = solveEditor.add.sprite(50, 50, bmd);
-    // item['name'].inputEnabled = true;
-    // item['name'].input.enableDrag();
-    // item['name'].input.enableSnap(50, 50, false, true); 
-    return item['name'];
+    return bmd;
+  }
+  function checkOverlap(spriteA, spriteB) {
+    var boundsA = spriteA.getBounds();
+    //console.log(boundsA['x']);
+    var boundsB = spriteB.getBounds();
+    return Phaser.Rectangle.containsRect(boundsA, boundsB);
   }
 })
 
